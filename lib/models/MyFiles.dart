0d2deee82ecb13ec2ae76c11f9bd0707d7c8bd26
Gabriel Honda <gabriel.honda@totemti.com.br>
2022-08-20 18:49:38 -0400

    refactor: adjust mock data of `MyFiles`

--- lib/models/MyFiles.dart
+++ lib/models/MyFiles.dart
@@ -1,54 +1,57 @@
-import 'package:admin/constants.dart';
 import 'package:flutter/material.dart';
+
+import '../constants.dart';
+import '../res/assets_res.dart';
 
 class CloudStorageInfo {
   final String svgSrc;
   final String title;
   final String totalStorage;
-  final int numOfFiels;
+  final int numOfFiles;
   final int percentage;
   final Color color;
 
-  CloudStorageInfo(
-      {this.svgSrc,
-      this.title,
-      this.totalStorage,
-      this.numOfFiels,
-      this.percentage,
-      this.color});
+  CloudStorageInfo({
+    this.svgSrc,
+    this.title,
+    this.totalStorage,
+    this.numOfFiles,
+    this.percentage,
+    this.color,
+  });
 }
 
-List demoMyFiels = [
+List myFilesData = [
   CloudStorageInfo(
     title: "Documents",
-    numOfFiels: 1328,
-    svgSrc: "assets/icons/Documents.svg",
+    numOfFiles: 1328,
+    svgSrc: AssetsRes.DOCUMENTS,
     totalStorage: "1.9GB",
     color: primaryColor,
     percentage: 35,
   ),
   CloudStorageInfo(
     title: "Google Drive",
-    numOfFiels: 1328,
-    svgSrc: "assets/icons/google_drive.svg",
+    numOfFiles: 1328,
+    svgSrc: AssetsRes.GOOGLE_DRIVE,
     totalStorage: "2.9GB",
-    color: Color(0xFFFFA113),
+    color: const Color(0xFFFFA113),
     percentage: 35,
   ),
   CloudStorageInfo(
     title: "One Drive",
-    numOfFiels: 1328,
-    svgSrc: "assets/icons/one_drive.svg",
+    numOfFiles: 1328,
+    svgSrc: AssetsRes.ONE_DRIVE,
     totalStorage: "1GB",
-    color: Color(0xFFA4CDFF),
+    color: const Color(0xFFA4CDFF),
     percentage: 10,
   ),
   CloudStorageInfo(
     title: "Documents",
-    numOfFiels: 5328,
-    svgSrc: "assets/icons/drop_box.svg",
+    numOfFiles: 5328,
+    svgSrc: AssetsRes.DROP_BOX,
     totalStorage: "7.3GB",
-    color: Color(0xFF007EE5),
+    color: const Color(0xFF007EE5),
     percentage: 78,
   ),
 ];
